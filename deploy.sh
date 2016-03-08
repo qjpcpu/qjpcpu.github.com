37e7ce0facb0ec9a3d32834f0946ff118e560133
qjpcpu <qjpcpu@gmail.com>
2016-03-08 13:03:24 +0800

    Auto commit.2016/03/08 13:03:24

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu
-(cd _deploy && git pull -u origin master)
+(cd _deploy && git pull  origin master)
 bundle exec rake generate
 bundle exec rake deploy
-git pull -u origin source
+git pull  origin source
 git add -A
 git commit -m "Auto commit.`date '+%Y/%m/%d %H:%M:%S'`"
 git push -u origin source
